1684a55877f36408d4f2150f6b68cec2905dc14a
Halqq <kauahmsilva@gmail.com>
2022-08-28 14:23:12 -0300

    Initial commit

---
+++ src/main/java/halq/itemframedupe/Main.java
@@ -0,0 +1,38 @@
+package halq.itemframedupe;
+
+import org.bukkit.Material;
+import org.bukkit.entity.ItemFrame;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author halq
+ * @apiNote speed run S2
+ * @since 28/08/2022
+ */
+
+public final class Main extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(this ,this);
+    }
+
+    @EventHandler
+    public void onFrame(EntityDamageByEntityEvent event){
+        ItemFrame f = (ItemFrame) event.getEntity();
+
+        if(f.getItem().getType() != Material.AIR){
+
+            if (0 + (int) (Math.random() * ((100 - 0) + 1)) <= getConfig().getInt("probability", 100)) {
+                f.getWorld().dropItemNaturally(f.getLocation().add(0,1,0), f.getItem());
+                f.getVelocity().zero();
+                }
+            }
+        }
+    }
+
+//  if (0 + (int) (Math.random() * ((100 - 0) + 1)) <= getConfig().getInt("probability", 100)) {
