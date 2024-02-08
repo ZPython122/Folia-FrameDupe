ed72505469df93c0b0c1450ac11e7f00aa662801
ZPython122 <133888052+ZPython122@users.noreply.github.com>
2024-02-08 17:34:47 -0500

    Update Main.java

--- src/main/java/halq/itemframedupe/Main.java
+++ src/main/java/halq/itemframedupe/Main.java
@@ -2,6 +2,7 @@ package halq.itemframedupe;
 
 import com.github.folia.api.FoliaPlayer;
 import com.github.folia.api.entity.FoliaItemFrame;
+import com.github.folia.api.event.entity.FoliaEntityDamageByEntityEvent;
 import com.github.folia.api.item.FoliaItemStack;
 import com.github.folia.api.plugin.FoliaPlugin;
 import com.github.folia.api.world.FoliaLocation;
@@ -13,19 +14,20 @@ public class Main extends FoliaPlugin implements FoliaListener {
     @Override
     public void onEnable() {
         this.probability = getConfig().getInt("probability", 100);
-        registerListener(this);
+        registerEvents(this);
     }
 
-    @Override
-    public void onFrameDamage(FoliaItemFrame frame, FoliaPlayer damager, float damage) {
+    @SubscribeEvent
+    public void onFrameDamage(FoliaEntityDamageByEntityEvent event) {
+        FoliaItemFrame frame = (FoliaItemFrame) event.getEntity();
+
         if (!frame.isEmpty() && Math.random() * 100 <= probability) {
             FoliaItemStack item = frame.getItem();
             FoliaLocation location = frame.getLocation().add(0, 1, 0);
             frame.getWorld().dropItem(location, item);
-            frame.setVelocity(0, 0, 0); // Zero out velocity
+            frame.setVelocity(0, 0, 0);
         }
     }
 }
-
 
 
